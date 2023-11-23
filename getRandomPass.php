c44e98f3934623b66818cf9c959b073f244ad829
Fabio <thebloodshadow14@gmail.com>
2023-11-23 17:55:50 +0100

    FIX

--- getRandomPass.php
+++ getRandomPass.php
@@ -2,7 +2,7 @@
 
 function getRandomPass($leng, $password_string) {
     $pass = "";
-    for ($i = 0; $i < $leng - 1; $i++){
+    for ($i = 0; $i < $leng; $i++){
         $random_int = random_int(0, strlen($password_string) - 1);
         $pass .= $password_string[$random_int];
     }
