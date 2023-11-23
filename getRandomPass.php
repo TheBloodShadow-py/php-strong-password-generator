9684dffd4f0e85b97561f51d4b9103353b0a5291
Fabio <thebloodshadow14@gmail.com>
2023-11-23 17:46:50 +0100

    ADD - getRandomPass

---
+++ getRandomPass.php
@@ -0,0 +1,10 @@
+<?php
+
+function getRandomPass($leng, $password_string) {
+    $pass = "";
+    for ($i = 0; $i < $leng - 1; $i++){
+        $random_int = random_int(0, strlen($password_string) - 1);
+        $pass .= $password_string[$random_int];
+    }
+    return $pass;
+}
--- index.php
+++ index.php
@@ -6,6 +6,25 @@
     <title>PHP Strong Password Generator</title>
 </head>
 <body>
+    <?php
+
+    require_once(__DIR__."/getRandomPass.php");
+
+    $password_string="aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789!@#$%^&*()";
+
+    $leng = $_GET["pass_len"] ?? 0;
+    ?>
+
+    <form method="get" action="">
+        <input min="1" type="number" name="pass_len">
+        <button>Genera password</button>
+        <?php
+
+    if ($leng ==! 0) {
+        echo "<p>".getRandomPass($leng, $password_string)."</p>";
+    }
+        ?>
+    </form>
     
 </body>
 </html>
